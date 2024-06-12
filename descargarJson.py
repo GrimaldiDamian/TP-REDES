eb6192677022e0c6b6a8b47c697029e6e4e2868c
Damián <grimaldidamian001@gmail.com>
2024-06-12 09:12:03 -0300

    Obtencion en formato diccionario del archivo json

--- descargarJson.py
+++ descargarJson.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 url = "https://api.nobelprize.org/v1/prize.json"
@@ -6,15 +7,18 @@ archivoJson = "prize.json"
 
 def descargarJson(url, save_path):
     try:
-        # Hacer la solicitud GET a la URL
         response = requests.get(url)
-        # Comprobar si la solicitud fue exitosa
         response.raise_for_status()
-        # Guardar el contenido JSON en un archivo
         with open(save_path, 'w', encoding='utf-8') as file:
             file.write(response.text)
         print(f"JSON descargado y guardado en {save_path}")
     except requests.exceptions.RequestException as e:
         print(f"Error descargando el JSON: {e}")
 
+def transformarloDic(archivo):
+    with open (archivo) as file:
+        dicJson = json.load(file)
+    return dicJson
+
 descargarJson(url,archivoJson)
+transformarloDic(archivoJson)
