8c9e111c521cf8541d2b59c6a415b2b070c7e665
Damián Grimaldi <grimaldidamian001@gmail.com>
2024-06-18 19:41:09 -0300

    Creacion del primer get

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+@app.get("/LeerArchivo")
+
+def LeerArchivo():
+    with open("prize.json", 'r') as file:
+        dicJson = json.load(file)
+    return dicJson
