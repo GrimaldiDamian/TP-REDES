6d16d8434043862821914038fcc3516e7197bfae
Damián <grimaldidamian001@gmail.com>
2024-06-20 22:22:23 -0300

    Creacion del archivo clienteApi

---
+++ clienteApi.py
@@ -0,0 +1,2 @@
+import requests
+url = "http://127.0.0.1:8000"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import Optional
 import json
 
 app = FastAPI()
